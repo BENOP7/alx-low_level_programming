63e5d700685a900089d8f418e928c40a6a03959c
benop7 <benjaminpetegnong@gmail.com>
2022-03-11 06:12:03 -0800

    6th task - C variables_if_else_while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,8 +9,9 @@ int main(void)
 {
 	int ch = 48;
 	int line_break = 10;
+	int m;
 
-	for (int m = ch; m > ch + 10; m++)
+	for (m = ch; m > ch + 10; m++)
 		putchar(m);
 	putchar(line_break);
 	return (0);
