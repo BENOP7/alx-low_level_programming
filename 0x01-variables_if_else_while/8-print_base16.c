af10975bd8ea64ba8b2f54ffd92549b86fa346f6
benop7 <benjaminpetegnong@gmail.com>
2022-03-11 06:20:54 -0800

    8th task - C variables_if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,10 +10,12 @@ int main(void)
 	int zero = 48;
 	int line_break = 10;
 	int first_letter = 97;
+	int m;
+	int n;
 
-	for (int n = zero; n < zero + 10; n++)
+	for (n = zero; n < zero + 10; n++)
 		putchar(n);
-	for (int m = first_letter; m < first_letter + 6; m++)
+	for (m = first_letter; m < first_letter + 6; m++)
 		putchar(m);
 	putchar(line_break);
 	return (0);
