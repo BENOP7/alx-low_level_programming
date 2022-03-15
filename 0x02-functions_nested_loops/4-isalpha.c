85b7db66251f47acf1d536ca002f7a4a6cccf1e7
benop7 <benjaminpetegnong@gmail.com>
2022-03-15 16:20:38 -0700

    4th task - C functions and nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks alphabets
+ * @c: character input as integer
+ * Return: 1 if c is a letter (i.e. uppercase or lowercase)
+ * Otherwise, returns 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
