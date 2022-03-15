fdff1d16ec42538b7b735c2d956ecafd2049372a
benop7 <benjaminpetegnong@gmail.com>
2022-03-15 16:05:35 -0700

    3rd task - C functions and nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a char is lower case
+ *
+ * Return: 1 if char is lower case
+ * Otherwise returns 0;
+ */
+int _islower(int c)
+{
+	if (c < 97 || c > 122)
+		return (0);
+	else
+		return (1);
+}
