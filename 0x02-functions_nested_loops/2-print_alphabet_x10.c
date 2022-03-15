ec3fde9976bf4ef08aa9ab9be3a4c21b8da222ff
benop7 <benjaminpetegnong@gmail.com>
2022-03-15 15:35:33 -0700

    2nd task - C functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(alphabets[i]);
+			_putchar('\n');
+		}
+	}
+}
