3a2ccd61317f3528f3c8747d1ab38e123c983ecb
benop7 <benjaminpetegnong@gmail.com>
2022-03-18 07:55:26 -0700

    2nd advanced task - C more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: input integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	int trunc = calc_trunc(n);
+	int tmp_dgt;
+
+	while (trunc != 0)
+	{
+		tmp_dgt = n / trunc;
+		n = n - trunc;
+		trunc /= 10;
+		print_digit(tmp_dgt);
+	}
+
+}
+
+/**
+ * print_digit - prints number digit by digit
+ * @digit: input digit
+ * Return: void
+ */
+void print_digit(int digit)
+{
+	if (digit < 0)
+	{
+		_putchar('0' + (digit * -1));
+	}
+	else
+	{
+		_putchar('0' + digit);
+	}
+}
+
+/**
+ * count_digit - counts digits in a number
+ * @n: number
+ * Return: number of digits in number
+ */
+int count_digit(int n)
+{
+	int count = 0;
+
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * calc_trunc - computes the initial trunc value of a number
+ * @n: number
+ * Return: value of trunc
+ */
+int calc_trunc(int n)
+{
+	int trunc = 1;
+	int p;
+
+	for (p = 0; p < count_digit(n); p++)
+	{
+		trunc *= 10;
+	}
+
+	return (trunc);
+}
