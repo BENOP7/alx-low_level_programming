725b12baaa1dab04d6f4ba5c9024138c3b29aba5
benop7 <benjaminpetegnong@gmail.com>
2022-03-18 17:48:16 -0700

    1st advanced task - more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the largest prime factor of a number
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long long num = 612852475143;
+	long div = 2;
+	long largest = 0;
+
+	while (num != 1)
+	{
+		if (num % div == 0)
+		{
+			num /= (long long) div;
+			largest = div;
+		}
+		div++
+	}
+
+	printf("%ld\n", largest);
+
+	return (0);
+}
