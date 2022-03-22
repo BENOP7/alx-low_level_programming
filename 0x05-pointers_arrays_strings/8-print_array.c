2a3d3f691ffa87a7a97623700740571415937c13
benop7 <benjaminpetegnong@gmail.com>
2022-03-21 17:35:38 -0700

    done 8th task - pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints first n elements of an array
+ * @n: number of elements to be printed
+ * @a: array
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int count = 0;
+
+	while (count == n)
+	{
+		if (count < n - 1)
+		{
+			printf("%d, ", a[count]);
+			continue;
+		}
+		
+		printf("%d\n", a[count]);
+	}
+}
