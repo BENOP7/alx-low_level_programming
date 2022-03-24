c7e5d35e95e3694dc73301ff12a9ea7978753098
benop7 <benjaminpetegnong@gmail.com>
2022-03-24 07:51:40 -0700

    done 2nd task - C more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,18 +11,21 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (dest[i] != '\0')
+	while (src[i] != '\0')
 	{
 		if (i < n)
 		{
 			dest[i] = src[i];
 		}
-		else if (src[i] != '\0')
-		{
-			dest[i] = '\0';
-		}
 		else
 			break;
+		i++;
+	}
+
+	while (dest[i] != '\0')
+	{
+		if (i < n)
+			dest[i] = '\0';
 		i++;
 	}
 
