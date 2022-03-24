ed1890c378a997d37fa9cbf72c21c12bcbfc0b18
benop7 <benjaminpetegnong@gmail.com>
2022-03-24 02:44:49 -0700

    done 6th task - C more pointers, arrays and strigns

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,33 +17,31 @@ char *cap_string(char *str)
 			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] = str[i] + 'A' - 'a';
 		}
-
 		if (str[i] == ',' || str[i] == '.' || str[i] == '(' ||
 			str[i] == ')' || str[i] == ';' || str[i] == '!' ||
 			str[i] == '\"' || str[i] == '?' || str[i] == '{' ||
-			str[i] == '}' || str[i] == ' ')
+			str[i] == '}' || str[i] == ' ' || str[i] == '\n' ||
+			str[i] == '\t')
 		{
-			if (!wflag)
+			if (wflag == 0)
 				wflag = 1;
+			i++;
+			continue;
 		}
-
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (wflag)
+			if (wflag == 1)
 			{
 				str[i] = str[i] + 'A' - 'a';
 				wflag = 0;
 			}
 		}
-
 		if (str[i] >= 'A' && str[i] <= 'Z')
 		{
-			if (wflag)
+			if (wflag == 1)
 				wflag = 0;
 		}
-
 		i++;
 	}
-
 	return (str);
 }
