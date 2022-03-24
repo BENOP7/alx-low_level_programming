94bf7df76d2beacfaf7f97f6d75eb97da2701693
benop7 <benjaminpetegnong@gmail.com>
2022-03-24 13:41:34 -0700

    2nd advanced task - c more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,14 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		putchar('-');
+	n = -n;
 	}
 
-	if (n / 10 == 0)
+	if (n / 10)
 	{
-		_putchar('0' + n % 10);
-		return;
+		print_number(n / 10);
 	}
 
-	print_number(n / 10);
-	_putchar('0' + n % 10);
-
-	return;
+	putchar('0' + n % 10);
 }
