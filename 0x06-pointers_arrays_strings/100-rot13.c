d30ce0f1afc493845aacf065188284538a3fff8d
benop7 <benjaminpetegnong@gmail.com>
2022-03-24 14:40:53 -0700

    1st advanced task - c more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,25 +10,23 @@ char *rot13(char *str)
 	int i = 0;
 	int j;
 	int k;
-	char lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-		'w', 'x', 'y', 'z'};
-	char upper[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+	char letters[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
 		'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-		'U', 'V', 'W', 'X', 'Y', 'Z'};
+		'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
+		'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z'};
+	char codes[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+	'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+	'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't','u', 'v', 'w', 'x',
+	'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
 	while (str[i] != '\0')
 	{
-		for (j = 0; j < 26; j++)
+		for (j = 0; j < 52; j++)
 		{
-			if (str[i] == lower[j])
+			if (str[i] == letters[j])
 			{
-				str[i] = lower[(j + 13) % 26];
-				break;
-			}
-			else if (str[i] == upper[j])
-			{
-				str[i] = upper[(j + 13) % 26];
+				str[i] = codes[j];
 				break;
 			}
 		}
