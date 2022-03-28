3150144b7de36a4cb3d24c20160ede2dc04a3bff
benop7 <benjaminpetegnong@gmail.com>
2022-03-28 07:20:04 -0700

    do 1st task - 0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,17 +3,17 @@
 /**
  * _memset - fills memory with a constant byte
  * @n: fills till the nth byte
- * @c: constant byte
+ * @b: constant byte
  * @s: memory location
  * Return: pointer to memory loaction
  */
-char *_memset(char *s, char c, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		s[i] = c;
+		s[i] = b;
 	}
 
 	return (s);
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,1 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(int);
 char *_memset(char *, char, unsigned int);
+
+#endif
