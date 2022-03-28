d592d0f46ce964d0a9f8b043fec5606f40ee9426
benop7 <benjaminpetegnong@gmail.com>
2022-03-28 09:44:58 -0700

    3rd task - 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of prefix substring
+ * @s: string
+ * @accept: substring
+ * Return: number of matching characters
+ */
+int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++) {
+		for (j = 0; accept[j] != str[i]; j++) {
+			if (accept[j] == '\0')
+				return (i);
+
+		}
+
+	}
+
+	return (i);
+}
