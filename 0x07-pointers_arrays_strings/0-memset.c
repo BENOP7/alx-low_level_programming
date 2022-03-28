61c4bf5717dcf62f5b74628df31330e9a1ad04c0
benop7 <benjaminpetegnong@gmail.com>
2022-03-28 15:37:13 -0700

    0th task - 0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
+ * _memset - fills memory with a constant bytes
  * @n: fills till the nth byte
  * @b: constant byte
  * @s: memory location
