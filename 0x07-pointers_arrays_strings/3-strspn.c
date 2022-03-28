1b16f8d7eed85bf659c424de0befaecccd03b60a
benop7 <benjaminpetegnong@gmail.com>
2022-03-28 09:47:16 -0700

    3rd task - 0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,8 +11,10 @@ int _strspn(char *s, char *accept)
 	int i;
 	int j;
 
-	for (i = 0; s[i] != '\0'; i++) {
-		for (j = 0; accept[j] != str[i]; j++) {
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != str[i]; j++)
+		{
 			if (accept[j] == '\0')
 				return (i);
 
