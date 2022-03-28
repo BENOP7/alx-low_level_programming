0cbb8c6201fdfd7473fb15ef91243bf46071f801
benop7 <benjaminpetegnong@gmail.com>
2022-03-28 16:11:57 -0700

    removing unwanted files

---
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ * a: two dimensional array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+	int l = 0;
+
+	while (
+	for (i = 0; i 
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,11 +6,11 @@
 int _putchar(int);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *, char);
+char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *, char *);
-int compare(char *, char *);
+char *_strstr(char *haystack, char needle*);
+int compare(char a*, char *b);
 #define NULL 0
 
 #endif
