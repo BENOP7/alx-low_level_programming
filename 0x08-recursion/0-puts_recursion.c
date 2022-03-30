93c233c87ec8dbedabf656c36bf3cad296ab78aa
benop7 <benjaminpetegnong@gmail.com>
2022-03-29 21:39:05 -0700

    0th task - 0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string to stdout
+ * @s: string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (!(*s))
+		return;
+
+	_putchar(*(++s));
+	_puts_recursion(s);
+}
