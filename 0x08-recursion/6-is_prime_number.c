33a3591e8a05485750b102392bcda16cc86bf070
benop7 <benjaminpetegnong@gmail.com>
2022-03-30 19:47:29 -0700

    6th task - 0x08-recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * is_prime_number - checks for a prime number
@@ -9,23 +9,25 @@ int is_prime_number(int n)
 {
 	if (n < 2)
 		return (0);
+	if (n == 2)
+		return (1);
 	if (n % 2 == 0)
 		return (0);
 	else
 	{
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n != 3)
 			return (0);
-		if (n % 5 == 0)
+		if (n % 5 == 0  && n != 5)
 			return (0);
-		if (n % 7 == 0)
+		if (n % 7 == 0  && n != 7)
 			return (0);
-		if (n % 13 == 0)
+		if (n % 13 == 0  && n != 13)
 			return (0);
-		if (n % 17 == 0)
+		if (n % 17 == 0  && n != 17)
 			return (0);
-		if (n % 19 == 0)
+		if (n % 19 == 0  && n != 19)
 			return (0);
-		if (n % 23 == 0)
+		if (n % 23 == 0  && n != 23)
 			return (0);
 	}
 	return (1);
