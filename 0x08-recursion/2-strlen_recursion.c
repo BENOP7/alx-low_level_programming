d360c675358d04c040cf8daabdacc5f1bb7846e2
benop7 <benjaminpetegnong@gmail.com>
2022-03-30 17:50:50 -0700

    2nd task - 0x08-recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - looks for the size of a string
+ * @s: string
+ * Return: returns the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (!(*s))
+		return (0);
+	return (1 + _strlen_recursion(s++));
+}
