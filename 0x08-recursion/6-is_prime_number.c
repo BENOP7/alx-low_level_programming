8995901ade57b78f2ae782207327ec961aa5d4ac
benop7 <benjaminpetegnong@gmail.com>
2022-03-30 19:51:13 -0700

    6th task - 0x08-recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,22 +13,20 @@ int is_prime_number(int n)
 		return (1);
 	if (n % 2 == 0)
 		return (0);
-	else
-	{
-		if (n % 3 == 0 && n != 3)
-			return (0);
-		if (n % 5 == 0  && n != 5)
-			return (0);
-		if (n % 7 == 0  && n != 7)
-			return (0);
-		if (n % 13 == 0  && n != 13)
-			return (0);
-		if (n % 17 == 0  && n != 17)
-			return (0);
-		if (n % 19 == 0  && n != 19)
-			return (0);
-		if (n % 23 == 0  && n != 23)
-			return (0);
+	if (n % 3 == 0 && n != 3)
+		return (0);
+	if (n % 5 == 0  && n != 5)
+		return (0);
+	if (n % 7 == 0  && n != 7)
+		return (0);
+	if (n % 13 == 0  && n != 13)
+		return (0);
+	if (n % 17 == 0  && n != 17)
+		return (0);
+	if (n % 19 == 0  && n != 19)
+		return (0);
+	if (n % 23 == 0  && n != 23)
+		return (0);
 	}
 	return (1);
 }
