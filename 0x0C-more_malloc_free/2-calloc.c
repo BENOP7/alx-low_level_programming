8b9d1d9fc53db2a1645f99f29bc6d7142573dbe6
benop7 <benjaminpetegnong@gmail.com>
2022-04-07 15:11:35 -0700

    2nd task : Correction - 0x0C-more_malloc_free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,12 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *p;
+
 	if (nmemb == 0 || size == 0)
 		return ((void *) 0);
-	return (malloc(nmemb * size));
+	p = malloc(nmemb * size);
+	if (!p)
+		return ((void *) 0);
+	return (p);
 }
