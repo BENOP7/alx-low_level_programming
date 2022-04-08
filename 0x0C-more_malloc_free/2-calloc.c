d777ce367e9984814d60a17e75fec802434956d9
benop7 <benjaminpetegnong@gmail.com>
2022-04-07 20:46:49 -0700

    Correction2 - 2nd task - C more mallo and free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,11 +10,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+
+	while (i < nmemb)
+	{
+		p[i] = 0;
+	}
 	return (p);
 }
