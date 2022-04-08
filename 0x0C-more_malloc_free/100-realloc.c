be54fbd71aaafd6bc1da6e8932266f7a5d18963e
benop7 <benjaminpetegnong@gmail.com>
2022-04-07 20:11:30 -0700

    1st advanced task - C more malloc and free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _realloc - reallocate memory block using malloc
+ * @ptr: pointer to memory block
+ * @old_size: old size
+ * @new_size: new size
+ * Return: pointer to new block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+	unsigned int i;
+
+	if (old_size == new_size)
+		return (ptr);
+	if (!ptr)
+	{
+		free(ptr);
+		p = malloc(new_size);
+
+		if (!p)
+			return ((void *) 0);
+		return (p);
+	}
+	if (ptr && new_size == 0)
+	{
+		free(ptr);
+		return ((void *) 0);
+	}
+
+	p = malloc(new_size);
+
+	if (!p)
+		return ((void *) 0);
+
+	for (i = 0; i < old_size; i++)
+	{
+		p[i] = ptr[i];
+	}
+
+	free(ptr);
+
+	return (p);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,6 @@ int *array_range(int, int);
 char *string_nconcat(char *, char *, unsigned int);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
-
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
