341776e0c455be21754e2657f098fc98a3506fae
benop7 <benjaminpetegnong@gmail.com>
2022-04-11 19:37:54 -0700

    3rd task - 0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,12 @@
  * @age: age of the dog
  * @owner: dog's owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *ower);
 void print_dog(struct dog *);
