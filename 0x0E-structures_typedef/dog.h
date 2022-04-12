4e9f4ccae4b1439ebf4b75531754869530ce13bf
benop7 <benjaminpetegnong@gmail.com>
2022-04-11 18:29:17 -0700

    1st task - 0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog - hold dog's informations
+ * description: Dog's info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *ower);
 
 #endif
