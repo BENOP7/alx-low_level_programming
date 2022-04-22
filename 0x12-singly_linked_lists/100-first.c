a5e340fddb52a81ca11681bca74c79bd9020b585
benop7 <benjaminpetegnong@gmail.com>
2022-04-21 19:07:36 -0700

    done - first advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * first - prints a string before the main function is called
  *
  * Return: void
  */
-void __attribute__((constructor)) first()
+void first(void)
 {
 	printf(
 	"You're bear! and yet, you must allow,\nI bore my house upon my back!\n"
