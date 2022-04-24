a40b1c431de4346f9efdbb87522307f2a3ef3639
benop7 <benjaminpetegnong@gmail.com>
2022-04-24 13:20:19 -0700

    task 8 - done

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <strlib.h>
+
+/**
+ * print_diagsums - print sum of diagonals of a square 2D array of integers
+ * @a: array
+ * @size: size of the array
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	if (!a || size < 1)
+		return;
+	while (i < size)
+	{
+		sum1 += a[i * (size + 1)];
+		sum2 += a[(size - 1) - i * (size - 1)]
+		i += 1;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
