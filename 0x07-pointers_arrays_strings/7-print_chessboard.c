d334ede6dec542cd11fcb45a5717c125a30126ee
benop7 <benjaminpetegnong@gmail.com>
2022-04-24 12:24:06 -0700

    done - task 7 - 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_chessboard - prints a chessboard
+ * a: two dimensional array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	size_t i = 0;
+	int j;
+	
+	if (!a)
+		return;
+	
+	while (i < (sizeof(*a) / sizeof(**a)))
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
