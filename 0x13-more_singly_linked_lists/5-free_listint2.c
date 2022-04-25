24b8390d7ba988a015d64bd095a3563e19418fcd
benop7 <benjaminpetegnong@gmail.com>
2022-04-25 01:14:05 -0700

    task 5 - done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list from memory
+ * @head: pointer to th first node
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = NULL;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
