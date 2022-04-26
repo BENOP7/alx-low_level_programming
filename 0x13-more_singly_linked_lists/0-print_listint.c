4d86debeb04326bbcf1990d6529be97e0555c271
benop7 <benjaminpetegnong@gmail.com>
2022-04-26 14:22:41 -0700

    0th task - correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,8 +7,11 @@
  */
 size_t print_listint(const listint_t *h)
 {
+	size_t len;
+
 	if (!h)
 		return (0);
 	printf("%d\n", h->n);
-	return (1 + print_listint(h->next));
+	len = 1 + print_listint(h->next);
+	return (len);
 }
