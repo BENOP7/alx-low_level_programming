197b1f2820dcda44b46fccf657c55976bf1b31cb
benop7 <benjaminpetegnong@gmail.com>
2022-05-04 21:34:23 -0700

    0th task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads text stored in a file
+ * filename: name of file to be read
+ * letters: number of letters to read and print
+ * Return: number of letters that were read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	
+	return 0;
+}
