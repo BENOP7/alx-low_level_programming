73004333ee6d8db838a7e93d3a4f3e60e302afbc
benop7 <benjaminpetegnong@gmail.com>
2022-05-27 13:51:29 +0100

    fix betty doc errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,8 +4,8 @@
 
 /**
  * read_textfile - reads text stored in a file
- * filename: name of file to be read
- * letters: number of letters to read and print
+ * @filename: name of file to be read
+ * @letters: number of letters to read and print
  * Return: number of letters that were read and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
